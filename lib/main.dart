602bc1a17a989c8d88c13ff6b84afa37db27e6d7
渡辺孝之 <na8esin.mpst@gmail.com>
2021-09-24 23:44:23 +0900

    DivElementとoverflowYを使ってスクロールさせる

--- lib/main.dart
+++ lib/main.dart
@@ -4,23 +4,26 @@ import 'dart:ui' as ui;
 import 'package:flutter/material.dart';
 
 void main() {
+  final anchor = AnchorElement(
+      href:
+          'https://twitter.com/na8esin/lists/1438080785391575041?ref_src=twsrc%5Etfw')
+    ..className = 'twitter-timeline'
+    ..text = 'hello hino city';
   // ignore: undefined_prefixed_name
   ui.platformViewRegistry
     ..registerViewFactory(
         'hello-html',
-        (int viewId) => AnchorElement(
-            href:
-                'https://twitter.com/na8esin/lists/1438080785391575041?ref_src=twsrc%5Etfw')
-          ..className = 'twitter-timeline'
-          ..text = 'hello'
-          ..style.width = 'auto'
-          ..style.height = 'auto')
+        (int viewId) => DivElement()
+          ..style.overflowY = 'scroll'
+          ..style.width = '300px'
+          ..style.height = '500px'
+          ..children = [anchor])
     ..registerViewFactory(
         'hello-javascript',
         (int viewId) => ScriptElement()
           ..src = 'https://platform.twitter.com/widgets.js'
-          ..style.width = '100%'
-          ..style.height = '100%'
+          ..style.width = '50%'
+          ..style.height = '50%'
           ..async = true);
 
   runApp(MaterialApp(
